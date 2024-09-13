2eada8a6a53b5e28d61cc019feee9b38478e13dd
isaaconek <onekdev5@gmail.com>
2024-09-13 12:29:59 +0100

    Amelioration des paragraphes dans la section about

--- src/assets/components/Body/main/sectionAbout/sectionAbout.jsx
+++ src/assets/components/Body/main/sectionAbout/sectionAbout.jsx
@@ -3,10 +3,10 @@ import GalleryLangages from "./galleryLangages"
 const SectionAbout = () => {
     return (
         <>
-            <section className="bg-principale flex flex-col sm:flex-row sm:p-20  ">
+            <section className="bg-principale flex flex-col sm:flex-row p-5 text-sm sm:p-20  ">
                 <div className="relative sm:w-texteSectionAcceuil">
-                    <h2 className="font-bold font-dortmund text-3xl">Venez apprendre avec moi les technos qui vous permetrons de rentrer dans l&apos;univers de la programmation informatique</h2>
-                    <p>Venez apprendre gratuitement des langages de programation tel  que le <strong>HTML</strong>, le<strong>CSS3</strong>, le <strong>JavaScript</strong>, le <strong>Php</strong> et plein d&apos;autres... </p>
+                    <h2 className="font-bold font-dortmund text-3xl text-center sm:text-left mb-5">Venez apprendre avec moi les technos qui vous permetrons de rentrer dans l&apos;univers de la programmation informatique</h2>
+                    <p className="w-80 sm:w-full text-center sm:text-left">Venez apprendre gratuitement des langages de programation tel  que le <strong>HTML</strong>, le<strong>CSS3</strong>, le <strong>JavaScript</strong>, le <strong>Php</strong> et plein d&apos;autres... </p>
                 </div>
                 <GalleryLangages/>
             </section>
