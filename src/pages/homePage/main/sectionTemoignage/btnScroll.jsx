c54c1053535753a096f805d301a6c58852f4828d
isaaconek <onekdev5@gmail.com>
2024-10-18 16:29:32 +0100

    Ajout de la classe hidden dans le composant btnScroll afin de le masquer dans sa version mobile

--- src/pages/homePage/main/sectionTemoignage/btnScroll.jsx
+++ src/pages/homePage/main/sectionTemoignage/btnScroll.jsx
@@ -7,7 +7,7 @@ const BtnScroll = ({scrollRef, scrollOffset}) => {
     }
     return (
         <button onClick={() => scroll(scrollOffset)} 
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-30 hover:bg-opacity-50 rounded-full p-2 z-10"
+        className="hidden sm:block absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-30 hover:bg-opacity-50 rounded-full p-2 z-10"
         >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
