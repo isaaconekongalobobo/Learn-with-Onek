43023890b3c2d7814a838dd77d72ac8b0fd825ad
isaaconek <onekdev5@gmail.com>
2024-10-29 07:40:33 +0100

    Ajout d'une animation pour le titre (Isaac onek)

--- src/pages/portfolioPage/section1/section1.jsx
+++ src/pages/portfolioPage/section1/section1.jsx
@@ -1,6 +1,21 @@
 import DescriptionText from "./descriptionText";
 import ItemCompetence from "./itemCompetence";
 import MyProfile from "./myProfile";
+import {motion} from 'framer-motion'
+// Animation pour le titre (Isaac onek)
+const titleVariant = {
+    hidden: {
+        opacity:0,
+        x:-30,
+    },
+    visible: {
+        opacity:1,
+        x: 1,
+        transition: {
+            delay: 1
+        }
+    }
+}
 
 const Section1 = () => {
     return (
@@ -9,10 +24,10 @@ const Section1 = () => {
                 <MyProfile/>  
                 <div className="">
                     <div>
-                        <h1 className=" text-[35px] sm:text-[50px] font-medium">Isaac Onek</h1>
+                        <motion.h1 variants={titleVariant} initial="hidden" animate="visible"  className="text-[35px] sm:text-[50px] font-medium">Isaac Onek</motion.h1>
                         <div>
-                            <ItemCompetence text={"Web developper | "} animationDelay={1} description="Le developpement web est..." />
-                            <ItemCompetence text={"Graphic designer | "} animationDelay={1.5} description="Le disgn graphique est..." />
+                            <ItemCompetence text={"Web developper | "} animationDelay={1.5} description="Le developpement web est..." />
+                            <ItemCompetence text={"Graphic designer | "} animationDelay={1.7} description="Le disgn graphique est..." />
                             <ItemCompetence text={"Web Designer "} animationDelay={2} description="Le web design est..." />
                         </div>
                         <DescriptionText text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos est similique vero iusto architecto distinctio sint non aperiam animi! Suscipit magnam quia vero, quibusdam mollitia consequuntur placeat omnis corporis soluta?" />
