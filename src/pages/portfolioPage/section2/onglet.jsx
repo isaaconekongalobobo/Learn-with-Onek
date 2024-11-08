3e707600eac3cd933a9b7e2746a8132518b69402
isaaconek <onekdev5@gmail.com>
2024-11-08 12:03:15 +0100

    Amelioration du style des boutons de navigation imbriquee pour mon portfolio

--- src/pages/portfolioPage/section2/onglet.jsx
+++ src/pages/portfolioPage/section2/onglet.jsx
@@ -4,9 +4,7 @@ import { NavLink } from "react-router-dom";
 const Onglet = ({to, text}) => {
     return (
         <NavLink to={to}>
-            <div>
-                <span className="">{text}</span>
-            </div>
+            <span className="p-2 rounded-full text-slate-800 font-medium">{text}</span>
         </NavLink>
     );
 }
--- src/pages/portfolioPage/section2/section2.jsx
+++ src/pages/portfolioPage/section2/section2.jsx
@@ -16,7 +16,7 @@ const sectionVariant = {
 
 const Section2 = () => {
     return (
-        <motion.section variants={sectionVariant} initial="hidden" animate="visible" className="pt-0 flex flex-col gap-5 sm:gap-10">
+        <motion.section variants={sectionVariant} initial="hidden" animate="visible" className="pt-5 flex flex-col gap-5 sm:gap-10 bg-white">
             <div>
                 <div className="flex justify-between p-10  sm:justify-center sm:gap-20 text-sm relative top-[-30px] sm:static ">
                     <Onglet to="about" text="A propos"/>
