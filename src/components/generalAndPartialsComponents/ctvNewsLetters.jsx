aa3b487a3be2a9215c25705c6b04410be08052b2
isaaconek <onekdev5@gmail.com>
2025-01-29 09:09:17 +0100

    Amelioration du CTV de la section hero de la page d'acceuil

--- src/components/generalAndPartialsComponents/ctvNewsLetters.jsx
+++ src/components/generalAndPartialsComponents/ctvNewsLetters.jsx
@@ -1,9 +1,7 @@
 import {motion} from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
 const btnVariant= {
-    whenHover: {
-        scale: 1.1
-    }
+    whenHover: { y:-5 }
 }
 
 const CtvNewsLetters = () => {
